df9f1feb1dd748da5e3c3f6b264770a104cee1f4
tarun.kumar <tarun.kumar@bigohtech.com>
2022-10-10 16:13:57 +0530

    First Commit

---
+++ src/ckeditor.js
@@ -0,0 +1,229 @@
+/**
+ * @license Copyright (c) 2014-2022, CKSource Holding sp. z o.o. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+import Alignment from "@ckeditor/ckeditor5-alignment/src/alignment.js";
+import Autoformat from "@ckeditor/ckeditor5-autoformat/src/autoformat.js";
+import Autosave from "@ckeditor/ckeditor5-autosave/src/autosave.js";
+import Bold from "@ckeditor/ckeditor5-basic-styles/src/bold.js";
+import Italic from "@ckeditor/ckeditor5-basic-styles/src/italic.js";
+import Subscript from "@ckeditor/ckeditor5-basic-styles/src/subscript.js";
+import Superscript from "@ckeditor/ckeditor5-basic-styles/src/superscript.js";
+import Underline from "@ckeditor/ckeditor5-basic-styles/src/underline.js";
+import BlockQuote from "@ckeditor/ckeditor5-block-quote/src/blockquote.js";
+import ClassicEditor from "@ckeditor/ckeditor5-editor-classic/src/classiceditor.js";
+import Essentials from "@ckeditor/ckeditor5-essentials/src/essentials.js";
+import FindAndReplace from "@ckeditor/ckeditor5-find-and-replace/src/findandreplace.js";
+import FontBackgroundColor from "@ckeditor/ckeditor5-font/src/fontbackgroundcolor.js";
+import FontColor from "@ckeditor/ckeditor5-font/src/fontcolor.js";
+import FontFamily from "@ckeditor/ckeditor5-font/src/fontfamily.js";
+import FontSize from "@ckeditor/ckeditor5-font/src/fontsize.js";
+import Heading from "@ckeditor/ckeditor5-heading/src/heading.js";
+import Highlight from "@ckeditor/ckeditor5-highlight/src/highlight.js";
+import HorizontalLine from "@ckeditor/ckeditor5-horizontal-line/src/horizontalline.js";
+import AutoImage from "@ckeditor/ckeditor5-image/src/autoimage.js";
+import Image from "@ckeditor/ckeditor5-image/src/image.js";
+import ImageCaption from "@ckeditor/ckeditor5-image/src/imagecaption.js";
+import ImageInsert from "@ckeditor/ckeditor5-image/src/imageinsert.js";
+import ImageResize from "@ckeditor/ckeditor5-image/src/imageresize.js";
+import ImageStyle from "@ckeditor/ckeditor5-image/src/imagestyle.js";
+import ImageToolbar from "@ckeditor/ckeditor5-image/src/imagetoolbar.js";
+import ImageUpload from "@ckeditor/ckeditor5-image/src/imageupload.js";
+import Indent from "@ckeditor/ckeditor5-indent/src/indent.js";
+import AutoLink from "@ckeditor/ckeditor5-link/src/autolink.js";
+import Link from "@ckeditor/ckeditor5-link/src/link.js";
+import LinkImage from "@ckeditor/ckeditor5-link/src/linkimage.js";
+import List from "@ckeditor/ckeditor5-list/src/list.js";
+import PageBreak from "@ckeditor/ckeditor5-page-break/src/pagebreak.js";
+import Paragraph from "@ckeditor/ckeditor5-paragraph/src/paragraph.js";
+import PasteFromOffice from "@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice.js";
+import SpecialCharacters from "@ckeditor/ckeditor5-special-characters/src/specialcharacters.js";
+import SpecialCharactersArrows from "@ckeditor/ckeditor5-special-characters/src/specialcharactersarrows.js";
+import SpecialCharactersCurrency from "@ckeditor/ckeditor5-special-characters/src/specialcharacterscurrency.js";
+import SpecialCharactersEssentials from "@ckeditor/ckeditor5-special-characters/src/specialcharactersessentials.js";
+import SpecialCharactersMathematical from "@ckeditor/ckeditor5-special-characters/src/specialcharactersmathematical.js";
+import SpecialCharactersText from "@ckeditor/ckeditor5-special-characters/src/specialcharacterstext.js";
+import Table from "@ckeditor/ckeditor5-table/src/table.js";
+import TableCellProperties from "@ckeditor/ckeditor5-table/src/tablecellproperties";
+import TableColumnResize from "@ckeditor/ckeditor5-table/src/tablecolumnresize.js";
+import TableToolbar from "@ckeditor/ckeditor5-table/src/tabletoolbar.js";
+import Base64UploadAdapter from "@ckeditor/ckeditor5-upload/src/adapters/base64uploadadapter.js";
+
+class Editor extends ClassicEditor {}
+
+// Plugins to include in the build.
+Editor.builtinPlugins = [
+  Alignment,
+  Autoformat,
+  AutoImage,
+  AutoLink,
+  Autosave,
+  Base64UploadAdapter,
+  BlockQuote,
+  Bold,
+  Essentials,
+  FindAndReplace,
+  FontBackgroundColor,
+  FontColor,
+  FontFamily,
+  FontSize,
+  Heading,
+  Highlight,
+  HorizontalLine,
+  Image,
+  ImageCaption,
+  ImageInsert,
+  ImageResize,
+  ImageStyle,
+  ImageToolbar,
+  ImageUpload,
+  Indent,
+  Italic,
+  Link,
+  LinkImage,
+  List,
+  PageBreak,
+  Paragraph,
+  PasteFromOffice,
+  SpecialCharacters,
+  SpecialCharactersArrows,
+  SpecialCharactersCurrency,
+  SpecialCharactersEssentials,
+  SpecialCharactersMathematical,
+  SpecialCharactersText,
+  Subscript,
+  Superscript,
+  Table,
+  TableCellProperties,
+  TableColumnResize,
+  TableToolbar,
+  Underline,
+];
+
+// Editor configuration.
+Editor.defaultConfig = {
+  plugins: [
+    Table,
+    TableToolbar,
+    ImageInsert,
+    AutoImage,
+    Link,
+    ImageInsert,
+    ImageResize,
+    ImageStyle,
+    AutoLink,
+    SpecialCharacters,
+    SpecialCharactersEssentials,
+    SpecialCharactersCurrency,
+    SpecialCharactersMathematical,
+  ],
+  toolbar: {
+    items: [
+      "heading",
+      "|",
+      "bold",
+      "italic",
+      "underline",
+      "link",
+      "bulletedList",
+      "numberedList",
+      "|",
+      "outdent",
+      "indent",
+      "blockQuote",
+      "alignment",
+      "specialCharacters",
+      "pageBreak",
+      "horizontalLine",
+      "|",
+      "findAndReplace",
+      "imageUpload",
+      "imageInsert",
+      "insertTable",
+      "fontBackgroundColor",
+      "fontColor",
+      "fontFamily",
+      "fontSize",
+      "highlight",
+      "subscript",
+      "superscript",
+      "undo",
+      "redo",
+    ],
+  },
+  language: "en",
+  image: {
+    resizeUnit: "px",
+    toolbar: [
+        "imageTextAlternative",
+        "imageStyle:inline",
+        "imageStyle:block",
+        "imageStyle:side",
+        "linkImage",
+      "resizeImage:50",
+      "resizeImage:75",
+      "resizeImage:original",
+    ],
+    resizeOptions: [
+      {
+        name: "resizeImage:original",
+        label: "Original",
+        value: null,
+      },
+      {
+        name: "resizeImage:100",
+        label: "100px",
+        value: "100",
+      },
+      {
+        name: "resizeImage:200",
+        label: "200px",
+        value: "200",
+      },
+      {
+        name: "resizeImage:original",
+        value: null,
+        icon: "original",
+      },
+      {
+        name: "resizeImage:50",
+        value: "50",
+        icon: "medium",
+      },
+      {
+        name: "resizeImage:75",
+        value: "75",
+        icon: "large",
+      },
+    ],
+  },
+  table: {
+    contentToolbar: [
+      "tableColumn",
+      "tableRow",
+      "mergeTableCells",
+      "tableCellProperties",
+    ],
+  },
+  link: {
+    addTargetToExternalLinks: true,
+    decorators: {
+      mode: "manual",
+      label: "Downloadable",
+      attributes: {
+        download: "download",
+      },
+    },
+    openInNewTab: {
+      mode: "manual",
+      label: "Open in a new tab",
+      defaultValue: true,
+      attributes: {
+        target: "_blank",
+        rel: "noopener noreferrer",
+      },
+    },
+  },
+};
+
+export default Editor;
